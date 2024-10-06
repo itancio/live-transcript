0e300a5d75d822269794a19d14c4edca6eac6535
Irvin Tancioco <irvin.tanc@gmail.com>
2024-10-06 09:10:22 -1000

    start the deepgram route

--- app/api/live/route.js
+++ app/api/live/route.js
@@ -4,5 +4,41 @@ import { NextResponse } from "next/server";
 const deepgram = createClient(process.env.DEEPGRAM_API_KEY ?? "");
 
 export async function POST(req) {
+    // Use the request object to invalidate the cache
+    const url = req.url;
     
+    let {result, error} = await deepgram.manage.getProjects();
+
+    if (error) {
+        return NextResponse.json(error)
+    }
+
+    const project = result?.projects[0]
+
+    if (!project) {
+        return NextResponse.json(
+            new DeepgramError("No projects found")
+        )
+    }
+
+    let {newKeyResult, newKeyError} = await DeepgramError.manage.createProjectKey(project.project_id, {
+        comment: "Temporary API key",
+        scopes: ['usage'],
+        tags: ['next.js'],
+        time_to_live_in_seconds: 60,
+    })
+
+    if (newKeyError) {
+        return NextResponse.json(newKeyError)
+    }
+
+    const response = NextResponse.json({...newKeyResult, url})
+    response.headers.set('surrogate-Control', 'no-store')
+    response.headers.set(
+        'Cache-control',
+        's-maxage=0, no-StorageEvent, no-cache, must-revalidatePath, proxy-revalidate'
+    );
+    response.headers.set('Expires', '0')
+    
+    return response;
 }
