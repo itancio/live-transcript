bcebe0d1f0086089d454429b926b9349b752e2b4
Irvin Tancioco <irvin.tanc@gmail.com>
2024-10-17 13:45:31 -0700

    wip for connecting mic to deepgram

--- app/hooks/useRecordVoice.js
+++ app/hooks/useRecordVoice.js
@@ -1,8 +1,20 @@
 import { useEffect, useState, useRef } from "react";
 
+// Callback function to get the result
+const blobToBase64 = (blob, apiRequest) => {
+  const reader = new FileReader();
+  reader.onload = function () {
+    const base64data = reader?.result?.split(",")[1];
+    apiRequest(base64data);
+  };
+  reader.readAsDataURL(blob);
+};
+
 export const useRecordVoice = () => {
   // State to hold the media recorder instance
   const [mediaRecorder, setMediaRecorder] = useState(null);
+
+  const [text, setText] = useState("");
 
   // State to track whether recording is currently in progress
   const [recording, setRecording] = useState(false);
@@ -14,6 +26,7 @@ export const useRecordVoice = () => {
   const startRecording = () => {
     if (mediaRecorder) {
       mediaRecorder.start();
+      console.log("Recording started...");
       setRecording(true);
     }
   };
@@ -22,9 +35,33 @@ export const useRecordVoice = () => {
   const stopRecording = () => {
     if (mediaRecorder) {
       mediaRecorder.stop(); 
+      console.log("Recording stopped...");
       setRecording(false);
     }
   };
+
+  const getText = async (base64data) => {
+    try {
+      const response = await fetch(`/api/live`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ audio: base64data }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error transcribing audio");
+      }
+
+      const {text} = await response.json();
+      setText(text)
+
+    }
+    catch (error) {
+      console.log("Error in retrieving text. ", error)
+    }
+  }
 
   // Function to initialize the media recorder with the provided stream
   const initialMediaRecorder = (stream) => {
@@ -36,15 +73,16 @@ export const useRecordVoice = () => {
     };
 
     // Event handler when data becomes available during recording
-    mediaRecorder.ondataavailable = (ev) => {
-      chunks.current.push(ev.data); // Storing data chunks
+    mediaRecorder.ondataavailable = (event) => {
+      chunks.current.push(event.data); // Storing data chunks
     };
 
     // Event handler when recording stops
     mediaRecorder.onstop = () => {
       // Creating a blob from accumulated audio chunks with WAV format
-      const audioBlob = new Blob(chunks.current, { type: "audio/wav" });
-      console.log(audioBlob, 'audioBlob')
+      const audioBlob = new Blob(chunks.current, { type: "audio/mp3" });
+      blobToBase64(audioBlob, getText);
+      console.log('audioBlob', audioBlob)
 
       // You can do something with the audioBlob, like sending it to a server or processing it further
       // If you plan to further process the audio or send it to a server, you can create a function to handle this 
@@ -55,22 +93,30 @@ export const useRecordVoice = () => {
     setMediaRecorder(mediaRecorder);
   };
 
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     navigator.mediaDevices
+  //       .getUserMedia({ audio: true })
+  //       .then(initialMediaRecorder)
+  //       .catch((error) => {
+  //         console.error("Microphone access denied:", error);
+  //       });
+  //   }
+    
+  //   return () => {
+  //     if (mediaRecorder) {
+  //       mediaRecorder.stream.getTracks().forEach(track => track.stop());
+  //     }
+  //   };
+  // }, [mediaRecorder]);
+
   useEffect(() => {
     if (typeof window !== "undefined") {
       navigator.mediaDevices
         .getUserMedia({ audio: true })
-        .then(initialMediaRecorder)
-        .catch((error) => {
-          console.error("Microphone access denied:", error);
-        });
+        .then(initialMediaRecorder);
     }
-    
-    return () => {
-      if (mediaRecorder) {
-        mediaRecorder.stream.getTracks().forEach(track => track.stop());
-      }
-    };
-  }, [mediaRecorder]);
+  }, []);
 
-  return { recording, startRecording, stopRecording };
+  return { text, recording, startRecording, stopRecording };
 };
--- app/page.js
+++ app/page.js
@@ -1,16 +1,18 @@
 'use client'
 
 import { useRecordVoice } from "./hooks/useRecordVoice";
-import { IconMicrophone } from "./components/IconMicrophone";
+import { Button } from '@mui/material';
+import MicIcon from '@mui/icons-material/Mic';
+
 
 export default function Home() {
-  const { startRecording, stopRecording } = useRecordVoice();
-  console.log(startRecording);
+  const { text, startRecording, stopRecording } = useRecordVoice();
+  console.log(text);
 
   return (
     <>
     {/* // Button for starting and stopping voice recording */}
-    <button
+    <Button
       onMouseDown={startRecording}    // Start recording when mouse is pressed
       onMouseUp={stopRecording}        // Stop recording when mouse is released
       onTouchStart={startRecording}    // Start recording when touch begins on a touch device
@@ -18,8 +20,8 @@ export default function Home() {
       className="border-none bg-transparent w-10"
     >
       {/* Microphone icon component */}
-      {/* <IconMicrophone /> */}
-    </button>
+      <MicIcon />
+    </Button>
     </>
   );
 };
